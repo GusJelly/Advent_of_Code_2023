48fd62dc25b54f6c63c7090fe9e6b9e95691f4f6
Gustavo Meireles <gugatpm@gmail.com>
2023-12-17 16:12:20 +0000

    I am terrible at programming wth brothers in christ please save my soul I am going to hell

--- day_2/main.go
+++ day_2/main.go
@@ -4,13 +4,49 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 )
+
+type Round struct {
+	red int
+	green int
+	blue int
+}
+
+type Game struct {
+	id int
+	rounds []Round
+}
 
 func main() {
 	lines := readFile("input")
-	for _, v := range lines {
-		fmt.Println(v)
+	for _, line := range lines {
+		fmt.Println()
+		fmt.Println("getGameId: ", getGameId(tokenize(line)))
+		fmt.Println("getRounds: ", getRounds(tokenize(line)))
 	}
+}
+
+// Gets the different rounds in a game
+func getRounds(tokens []string) []Round {
+	var rounds []Round
+
+	for i := 2; i < len(tokens); i++ {
+		fmt.Printf("%s ", tokens[i])
+	}
+
+	return rounds
+}
+
+// Gets the game's id from the game's tokens slice
+func getGameId(tokens []string) int {
+	id, err := strconv.Atoi(strings.Trim(tokens[1], ":"))
+	if err != nil {
+		panic(err)
+	}
+
+	return id
 }
 
 // Loads a given file into a string slice
@@ -32,4 +68,9 @@ func readFile(name string) []string {
 	}
 
 	return lines
+}
+
+func tokenize(line string) []string {
+	tokens := strings.Split(line, " ")
+	return tokens
 }
