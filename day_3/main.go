17747cf678c6e849357ef8732184415fd2931b05
Gustavo Meireles <gugatpm@gmail.com>
2023-12-18 14:07:44 +0000

    Started day 3

---
+++ day_3/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+    "os"
+    "bufio"
+)
+
+func main() {
+
+}
+
+func loadFile(name string) []string {
+	// Load the file from the system
+	file, err := os.Open(name)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	// Create scanner to read the file
+	var lines []string
+	scanner := bufio.NewScanner(file)
+
+	// Put all the lines inside the file into a slice
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+
+	return lines
+}
---
+++ day_3/schematic.go
@@ -0,0 +1,11 @@
+package main
+
+var directions = map[string] []int {
+    "up": {0, 1},
+    "down": {0, -1},
+    "left": {-1, 0},
+    "right": {0, 1},
+}
+
+func checkSymbols([][]string) {
+}
